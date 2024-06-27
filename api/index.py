be3fb8aa34694e01e0f1e286e093fd4cd01bcc81
CatStack496 <73456721+CatStack496@users.noreply.github.com>
2024-06-26 21:10:04 -0400

    bruh

--- api/index.py
+++ api/index.py
@@ -1,38 +1,15 @@
-from flask import Flask, url_for
+from flask import Flask, send_file
 from datetime import datetime
 
 app = Flask(__name__)
 
+def sparkleGif():
+    return "./static/" + datetime.now().strftime('%A') + ".gif"
+
 @app.route('/')
-def home():
-    # Get the current day of the week
-    day_of_week = datetime.now().strftime('%A')
-    
-    # Define the HTML content with the day of the week replaced
-    html_content = f"""
-    <html style="height: 100%">
-      <head>
-        <meta name="viewport" content="width=device-width, minimum-scale=0.1" />
-        <title>Sparkle on! It's {day_of_week}</title>
-      </head>
-      <body style="margin: 0px; height: 100%; background-color: rgb(14, 14, 14)">
-        <img
-          style="
-            display: block;
-            -webkit-user-select: none;
-            margin: auto;
-            background-color: hsl(0, 0%, 90%);
-            transition: background-color 300ms;
-          "
-          src="{ url_for('static', filename=day_of_week + '.gif') }"
-          width="945"
-          height="945"
-        />
-      </body>
-    </html>
-    """
-    
-    return html_content
+def sparkleOn():
+    image = sparkleGif()
+    return send_file(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
