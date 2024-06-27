9f6f86b6582b10cf9f3d10e23a1d54263f131c02
CatStack496 <73456721+CatStack496@users.noreply.github.com>
2024-06-26 21:50:13 -0400

    bruh2

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 from flask import Flask, send_file
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
 def sparkleGif():
-    return "./static/" + datetime.now().strftime('%A') + ".gif"
+    eastern = pytz.timezone('US/Eastern')
+    utc_now = datetime.now(pytz.utc)
+    est_now = utc_now.astimezone(eastern)
+    current_day_est = est_now.strftime("%A")
+    return "./static/" + current_day_est + ".gif"
 
 @app.route('/')
 def sparkleOn():
